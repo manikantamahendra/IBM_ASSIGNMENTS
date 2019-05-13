837f73ee1b83d9f505b8ad8fc7eaca7743346f9c
manikantamahendra <50027736+manikantamahendra@users.noreply.github.com>
2019-05-13 16:02:21 +0530

    Delete CompareString.java

--- CompareString.java
+++
@@ -1,8 +0,0 @@
-class CompareString{
-	public static void main(String[] args) {
-		String str1="welcome to java world and python";
-		String str2="welcome to java world ";
-	     int result=str1.compareToIgnoreCase(str2);
-		System.out.print(result);
-	}
-}
