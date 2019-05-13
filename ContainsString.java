984952e19856178af1d9c866b7cc5e70aec0475d
manikantamahendra <50027736+manikantamahendra@users.noreply.github.com>
2019-05-13 16:06:33 +0530

    Delete ContainsString.java

--- ContainsString.java
+++
@@ -1,8 +0,0 @@
-class ContainsString{
-	public static void main(String[] args) {
-		String str1="welcome to java world";
-		String str2="welcome to ";
-	    Boolean result=str1.contains(str2);
-		System.out.print(result);
-	}
-}
